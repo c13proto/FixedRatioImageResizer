cc70cb341df5727761f117005fe8c608e39db5f9
c13proto <c13proto@aqua.ocn.ne.jp>
2018-11-06 01:11:32 +0900

    init

--- FixedRatioImageResizer/Form1.Designer.cs
+++ FixedRatioImageResizer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FixedRatioImageResizer
 {
-    partial class Form1
+    partial class FixedRatioImageResizer
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,13 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_open = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_width = new System.Windows.Forms.TextBox();
+            this.textBox_height = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.textBox_frame = new System.Windows.Forms.TextBox();
+            this.label_frame = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_r = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_g = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(12, 12);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(91, 20);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "open images";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "height";
+            // 
+            // textBox_width
+            // 
+            this.textBox_width.Location = new System.Drawing.Point(16, 50);
+            this.textBox_width.Name = "textBox_width";
+            this.textBox_width.Size = new System.Drawing.Size(30, 19);
+            this.textBox_width.TabIndex = 3;
+            this.textBox_width.Text = "1280";
+            // 
+            // textBox_height
+            // 
+            this.textBox_height.Location = new System.Drawing.Point(54, 50);
+            this.textBox_height.Name = "textBox_height";
+            this.textBox_height.Size = new System.Drawing.Size(30, 19);
+            this.textBox_height.TabIndex = 4;
+            this.textBox_height.Text = "1280";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 149);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(91, 20);
+            this.button_start.TabIndex = 5;
+            this.button_start.Text = "start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // textBox_frame
+            // 
+            this.textBox_frame.Location = new System.Drawing.Point(12, 87);
+            this.textBox_frame.Name = "textBox_frame";
+            this.textBox_frame.Size = new System.Drawing.Size(30, 19);
+            this.textBox_frame.TabIndex = 8;
+            this.textBox_frame.Text = "1";
+            // 
+            // label_frame
+            // 
+            this.label_frame.AutoSize = true;
+            this.label_frame.Location = new System.Drawing.Point(10, 72);
+            this.label_frame.Name = "label_frame";
+            this.label_frame.Size = new System.Drawing.Size(62, 12);
+            this.label_frame.TabIndex = 7;
+            this.label_frame.Text = "frameWidth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "background (r,g,b)";
+            // 
+            // textBox_r
+            // 
+            this.textBox_r.Location = new System.Drawing.Point(12, 124);
+            this.textBox_r.Name = "textBox_r";
+            this.textBox_r.Size = new System.Drawing.Size(23, 19);
+            this.textBox_r.TabIndex = 10;
+            this.textBox_r.Text = "255";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(109, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 53);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(70, 124);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(23, 19);
+            this.textBox_b.TabIndex = 12;
+            this.textBox_b.Text = "255";
+            // 
+            // textBox_g
+            // 
+            this.textBox_g.Location = new System.Drawing.Point(41, 124);
+            this.textBox_g.Name = "textBox_g";
+            this.textBox_g.Size = new System.Drawing.Size(23, 19);
+            this.textBox_g.TabIndex = 13;
+            this.textBox_g.Text = "255";
+            // 
+            // FixedRatioImageResizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(280, 179);
+            this.Controls.Add(this.textBox_g);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox_r);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_frame);
+            this.Controls.Add(this.label_frame);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox_height);
+            this.Controls.Add(this.textBox_width);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_open);
+            this.Name = "FixedRatioImageResizer";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "FixedRatioImageResizer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_width;
+        private System.Windows.Forms.TextBox textBox_height;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.TextBox textBox_frame;
+        private System.Windows.Forms.Label label_frame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_r;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_g;
     }
 }
 
--- FixedRatioImageResizer/Form1.cs
+++ FixedRatioImageResizer/Form1.cs
@@ -10,11 +10,83 @@ using System.Windows.Forms;
 
 namespace FixedRatioImageResizer
 {
-    public partial class Form1 : Form
+    public partial class FixedRatioImageResizer : Form
     {
-        public Form1()
+        public List<string> ImageFiles;
+        public FixedRatioImageResizer()
         {
             InitializeComponent();
         }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+
+            };
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+            //複数のファイルを選択できるようにする
+            ofd.Multiselect = true;
+            // フィルタ
+            ofd.Filter = "画像ファイル|*.bmp;*.gif;*.jpg;*.png|全てのファイル|*.*";
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (ImageFiles != null) ImageFiles.Clear();
+                ImageFiles = new List<string>();
+                //OKボタンがクリックされたとき、選択されたファイル名をすべて表示する
+                foreach (string fn in ofd.FileNames)
+                    ImageFiles.Add(fn);
+                if (ImageFiles.Count > 0) {
+                    Image src =  Image.FromFile(ImageFiles[0]);
+                    pictureBox1.Width = src.Width;
+                    pictureBox1.Height = src.Height;
+                    pictureBox1.Image=src;
+                    this.Width = src.Width + 200;
+                    this.Height = src.Height + 50;
+                    
+                }
+
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void makeAmazonSellerImage2(string src, int dstWidth, int dstHeight)
+        //{
+        //    Bitmap orgImage = new Bitmap(src);
+
+        //    Bitmap baseImage = new Bitmap(dstWidth, dstHeight);
+
+        //    int resizeWidth, resizeHeight;
+        //    if (dstWidth * orgImage.Height / orgImage.Width > dstHeight)//高さで合わせるパターン
+        //    {
+        //        resizeHeight = dstHeight;
+        //        resizeWidth = resizeHeight * orgImage.Width / orgImage.Height;
+        //    }
+        //    else
+        //    {
+        //        resizeWidth = dstWidth;
+        //        resizeHeight = resizeWidth * orgImage.Height / orgImage.Width;
+        //    }
+        //    Bitmap resizeBmp = new Bitmap(resizeWidth, resizeHeight);
+        //    Cv2.Resize(orgImage, orgImage, new OpenCvSharp.Size(resize_width, resize_height));
+
+        //    int paddWidth = (baseImage.Width - orgImage.Width) / 2;
+        //    int paddHeight = (baseImage.Height - orgImage.Height) / 2;
+        //    Mat ROI = new Mat(baseImage, new Rect(paddWidth, paddHeight, resize_width, resize_height));
+
+        //    orgImage.CopyTo(ROI);
+        //    Cv2.ImShow(src, baseImage);
+
+
+        //    orgImage.Release();
+        //    baseImage.Release();
+        //    ROI.Release();
+        //}
     }
 }
